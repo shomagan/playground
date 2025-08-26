957ce4c480269a5d1a8b8286313bc58d115880c0
Shoma Gan <shomagan@gmail.com>
2025-08-26 15:41:10 +0200

    xml reader

--- playground/cpp/xml/demo.cpp
+++ playground/cpp/xml/demo.cpp
@@ -1,4 +1,4 @@
-#include "xmlDict.h"
+#include "xmlDict.hpp"
 #include <iostream>
 
 int main() {
@@ -13,7 +13,7 @@ int main() {
     
     // 1. Simple string access
     std::cout << "1. Accessing guest interface: ";
-    auto guest_interface = xml[std::string("config")]["guest"][0]["interface"];
+    auto guest_interface = xml["config"]["host"]["network"]["interfaces"]["interface"][0]["@name"];
     if (guest_interface.isValid()) {
         std::cout << guest_interface.asString() << std::endl;
     } else {
@@ -22,45 +22,29 @@ int main() {
     
     // 2. Accessing attributes with @ prefix
     std::cout << "2. Accessing guest name attribute: ";
-    auto guest_name = xml[std::string("config")]["guest"][0]["@name"];
+    auto guest_name = xml["config"]["guest"][0]["@name"];
     if (guest_name.isValid()) {
         std::cout << guest_name.asString() << std::endl;
     } else {
         std::cout << "not found" << std::endl;
     }
     
-    // 3. Accessing nested VLAN information
-    std::cout << "3. Accessing first interface name: ";
-    auto interface_name = xml[std::string("config")]["firewall"]["interface"][0]["@name"];
-    if (interface_name.isValid()) {
-        std::cout << interface_name.asString() << std::endl;
-    } else {
-        std::cout << "not found" << std::endl;
-    }
-    
+   
     // 4. Accessing VLAN ID
     std::cout << "4. Accessing VLAN ID: ";
-    auto vlan_id = xml[std::string("config")]["firewall"]["interface"][0]["vlan"][0]["vlan"][0]["@id"];
+    auto vlan_id = xml["config"]["host"]["network"]["firewall"]["vlans"]["vlan"][0];
     if (vlan_id.isValid()) {
         std::cout << vlan_id.asString() << std::endl;
+        vlan_id.print();
     } else {
         std::cout << "not found" << std::endl;
     }
     
     // 5. Accessing VLAN PCP
     std::cout << "5. Accessing VLAN PCP: ";
-    auto vlan_pcp = xml[std::string("config")]["firewall"]["interface"][0]["vlan"][0]["vlan"][0]["@pcp"];
+    auto vlan_pcp = xml["config"]["host"]["network"]["mapping"]["vlans"]["vlan"][0]["@pcp"];
     if (vlan_pcp.isValid()) {
         std::cout << vlan_pcp.asString() << std::endl;
-    } else {
-        std::cout << "not found" << std::endl;
-    }
-    
-    // 6. Accessing server IP
-    std::cout << "6. Accessing server IP: ";
-    auto server_ip = xml[std::string("config")]["firewall"]["interface"][0]["vlan"][0]["server_ip"];
-    if (server_ip.isValid()) {
-        std::cout << server_ip.asString() << std::endl;
     } else {
         std::cout << "not found" << std::endl;
     }
@@ -68,7 +52,7 @@ int main() {
     // 7. Loop through multiple VLANs
     std::cout << "7. All VLANs in first interface:" << std::endl;
     for (int i = 0; i < 10; ++i) { // Try up to 10 VLANs
-        auto vlan_name = xml[std::string("config")]["firewall"]["interface"][0]["vlan"][i]["@name"];
+        auto vlan_name = xml["config"]["host"]["network"]["mapping"]["vlans"]["vlan"][i]["@id"];
         if (vlan_name.isValid()) {
             std::cout << "   VLAN " << i << ": " << vlan_name.asString() << std::endl;
         } else {
@@ -78,16 +62,12 @@ int main() {
     
     // 8. Convert to integer
     std::cout << "8. VLAN ID as integer: ";
-    auto vlan_id_int = xml[std::string("config")]["firewall"]["interface"][0]["vlan"][0]["vlan"][0]["@id"].asInt();
+    auto vlan_id_int = xml["config"]["host"]["network"]["mapping"]["vlans"]["vlan"][2]["@id"].asInt();
     std::cout << vlan_id_int << std::endl;
-    
-    std::cout << "\n=== Comparison ===" << std::endl;
-    std::cout << "Python: xml['config']['firewall']['interface'][0]['vlan'][0]['vlan'][0]['@id']" << std::endl;
-    std::cout << "C++:    xml[\"config\"][\"firewall\"][\"interface\"][0][\"vlan\"][0][\"vlan\"][0][\"@id\"].asString()" << std::endl;
     
     // 9. Error handling example
     std::cout << "\n9. Non-existent path handling: ";
-    auto non_existent = xml[std::string("config")]["nonexistent"]["path"][999]["@attr"];
+    auto non_existent = xml["config"]["nonexistent"]["path"][999]["@attr"];
     if (non_existent.isValid()) {
         std::cout << non_existent.asString() << std::endl;
     } else {
